06e289d3771c17d3f1136a69988546b89155ddc4
tyomka.info <tyomka.info@gmail.com>
2023-01-31 13:07:32 +0100

    done all

--- js/scripts.js
+++ js/scripts.js
@@ -160,12 +160,33 @@ function initMarquee() {
         });
     };
 }(jQuery));
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: [ '', ' ' ],
 
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1100:{
+                items:3
+            }
+        }
+    });
 
-$("a.scroll-to").on("click", function(e){
-    e.preventDefault();
-    var anchor = $(this).attr('href');
-    $('html, body').stop().animate({
-        scrollTop: $(anchor).offset().top - 10
-    }, 800);
+    $("a.scroll-to").on("click", function(e){
+        e.preventDefault();
+        var anchor = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(anchor).offset().top - 10
+        }, 800);
+    });
 });
